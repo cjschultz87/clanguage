89c74bf06dd0b5b4da0729f51965d611651c5000
CJ <131416165+cjschultz87@users.noreply.github.com>
2026-04-17 19:17:31 -0400

    uploaded romeoarray.c
    the program produces a randomized list of integers from a minimum to a maximum value; can be used in shell scripts, e.g. to randomize port scans in conjunction with openings in a firewall.

---
+++ romeoarray.c
@@ -0,0 +1,91 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "time.h"
+
+#include "strings.h"
+
+typedef char *str;
+typedef unsigned char BYTE;
+typedef unsigned int ULONG;
+
+void main(int argc, str* argv)
+{
+	if (argc != 4)
+	{
+		printf("<minimum> <maximum> <seperator char>");
+		
+		goto endoffunction;
+	}
+	
+	ULONG minimum = strToL(argv[1],10);
+	
+	ULONG maximum = strToL(argv[2],10);
+	
+	str separator = argv[3];
+	
+	ULONG aL = maximum - minimum + 1;
+	
+	BYTE *alpha = calloc(aL, sizeof(BYTE));
+	
+	BYTE *alpha_prime;
+	
+	for (int i = minimum; i <= maximum; i++)
+	{
+		alpha[i - minimum] = i;
+	}
+	
+	ULONG aL_prime = aL;
+	
+	for (int i = 0; i < aL; i++)
+	{
+		srand(clock());
+		
+		ULONG iota = (ULONG)rand() % aL_prime;
+		
+		printf("%u",alpha[iota]);
+		
+		aL_prime--;
+		
+		alpha_prime = calloc(aL_prime, sizeof(BYTE));
+		
+		ULONG iota_prime = 0;
+		
+		for (int i_0 = 0; i_0 < aL_prime + 1; i_0++)
+		{
+			if (i_0 == iota)
+			{	
+				iota_prime++;
+				
+				goto skipfor0;
+			}
+			
+			alpha_prime[i_0 - iota_prime] = alpha[i_0];
+			
+			skipfor0:{};
+		}
+		
+		alpha = calloc(aL_prime, sizeof(BYTE));
+		
+		for (int i_0 = 0; i_0 < aL_prime; i_0++)
+		{
+			alpha[i_0] = alpha_prime[i_0];
+		}
+		
+		if (i < aL - 1)
+		{
+			printf("%s",separator);
+		}
+		
+		srand(clock());
+		
+		ULONG sleep_val = (ULONG)rand() % 15000;
+		
+		for (int i_0 = 0; i_0 < sleep_val; i_0++)
+		{
+			
+		}
+	}
+	
+	endoffunction:{};
+}
